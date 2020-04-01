441723c107b698a817bc5929e84de080a8a6ee5d
1 <1@DESKTOP-4MJ3ERJ>
2020-04-01 20:36:04 +0300

    готовы адрес и паспорт, почти готов вариант персоны

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -14,55 +14,53 @@ public class Main {
                 
         System.out.println("тестовый вывод");
 
-        Person ivanov = new Person("Ivan", "Ivanov", "Ivanovich", "ch"); //созд оъекта с 1 методом И Ф О -ввод лишний переменных, надо откорр
-        String fullDataIvanov = ivanov.getFullName1();
         
-        Person smith = new Person("John", "Edvard", "Smith ", "Kent"); //созд оъекта с 2 методом И 2И Ф -ввод лишний переменных, надо откорр
-        String fullDataSmith = smith.getFullName2();  
         
-        Person john = new Person("John" , "Doe", "Jack", "Iasasdh"); //тестовый
-        String fullNameDoe = john.getFullName2();
+        Person ivanov = new Person("Ivan", "Ivanov", "Ivanovich"); //созд оъекта с 1 методом И Ф О -ввод лишний переменных, надо откорр
+        String nameIvanov = ivanov.getFullName1();
         
-        Person sara = new Person("Sara" , "Black", "Ivsaddiasch", "Iasdasdwvich"); //созд оъекта с 3 методом И Ф -ввод лишний переменных, надо откорр
-        String fullDataSara = sara.getFullName3();
+        Person smith = new Person("John", "Smith", "Edvard"); //созд оъекта с 2 методом И 2И Ф -ввод лишний переменных, надо откорр
+        String nameSmith = smith.getFullName2();  
         
+        Person john = new Person("John" , "Doe"); //тестовый
+        String nameDoe = john.getFullName3();
+        
+            
                     
-        System.out.println(fullDataIvanov);
-        System.out.println(fullDataSmith);
-        
-        System.out.println(fullNameDoe);
-        System.out.println(fullDataSara);
-        
+        System.out.println(nameIvanov);
+        System.out.println(nameSmith);
+        System.out.println(nameDoe);
+                
         Address ivanovAddress = new Address("Russia", "Moscow", "Lenina", "9", "4" , "111222");
         String fullAddress = ivanovAddress.getFullAddress();
         
         Address smithAddress = new Address("USA", "NY", "Bush str", "7", "4" , "65464654");
-        String fullAddress2 = smithAddress.getFullAddress2();
+        String fullAddress2 = smithAddress.getFullAddress();
         
-        Address saraAddress = new Address("country", "city", "street", "building", "nomber" , "indecs");
-        String fullAddress3 = saraAddress.getFullAddress3();
+        Address doeAddress = new Address("country", "city", "street", "building", "nomber" , "indecs");
+        String fullAddress3 = doeAddress.getFullAddress();
         
         
-        System.out.println(fullDataIvanov + fullAddress);
-        System.out.println(fullDataSmith + fullAddress2);
-        System.out.println(fullDataSara + fullAddress3);
+        System.out.println(nameIvanov + "зарегистрирован " + fullAddress);
+        System.out.println(nameSmith + "зарегистрирован " + fullAddress2);
+        System.out.println(nameDoe + "зарегистрирован " + fullAddress3);
       
         //создать метод - возвр имя + адрес
         
         
         
         //System.out.println(name);
-// test1//
         /*
          * TODO(Студент): Создайте экземпляры класса 'Person'
          *
          * 1. Проинициализируейте переменную 'ivanov', так,
          *    чтобы она адресовала объект типа 'Person'
          *    описывающий человека по имени 'Иванов Иван Иванович'.
-         *
+         *------------------------ВЫПОЛНЕНО----------------
          * 2. Проинициализируейте переменную 'smith', так,
          *    чтобы она адресовала объект типа 'Person'
          *    описывающий человека по имени 'John Edvard Smith'.
+        ------------------------ВЫПОЛНЕНО----------------
          */
 
         /*
@@ -71,18 +69,19 @@ public class Main {
          * 1. Строковую переменную, содержащую полное имя
          *    человека, описанного объектом, адресованным
          *    переменной 'ivanov'.
-         *
+         *------------------------ВЫПОЛНЕНО----------------
          * 2. Строковую переменную, содержащую полное имя
          *    человека, описанного объектом, адресованным
          *    переменной 'smith'.
-         *
+         *------------------------ВЫПОЛНЕНО----------------
          * 3. Строковую переменную, содержащую адрес проживания
          *    человека, описанного объектом, адресованным
          *    переменной 'ivanov'.
-         *
+         *------------------------ВЫПОЛНЕНО----------------
          * 4. Строковую переменную, содержащую адрес проживания
          *    человека, описанного объектом, адресованным
          *    переменной 'smith'.
+        ------------------------ВЫПОЛНЕНО----------------
          */
 
         /*
@@ -93,7 +92,5 @@ public class Main {
          */
     }
 
-    private static void getFullAddress() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+   
 }
